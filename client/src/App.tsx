3c3b8b5f6453387c15f10daa5d7c470a1fbf7c90
Stan-breaks <stanleymwendwa03@gmail.com>
2024-03-15 12:33:16 +0300

    init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
+import AppRoutes from "./routes/index";
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>...</h1>
+      <AppRoutes />
+    </>
+  );
 }
 
 export default App;
-
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,1 +1,22 @@
+import { Route, Routes } from "react";
+import Home from "../pages/Home";
+import Landing from "../pages/Landing";
+import NotFound from "../pages/NotFound";
+import Profile from "../pages/Profile";
+import Projects from "../pages/Projects";
+import Tasks from "../pages/Tasks";
 
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/tasks" element={<Tasks />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
